132feaec783e8c90aa331ecbc723255e6034eca4
CristianSandyFlores <32717021+CristianSandyFlores@users.noreply.github.com>
2021-09-28 16:44:44 -0400

    commit 28/07/21

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,7 +33,17 @@
   <link rel="stylesheet" href="{{ asset('frontend') }}/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="{{ asset('frontend') }}/../../dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="{{ asset('frontend') }}/dist/css/skins/_all-skins.min.css">
+  <!-- bootstrap datepicker -->
+  <link rel="stylesheet" href="{{ asset('frontend') }}/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <!-- iCheck for checkboxes and radio inputs -->
+  <link rel="stylesheet" href="{{ asset('frontend') }}/plugins/iCheck/all.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="{{ asset('frontend') }}/bower_components/select2/dist/css/select2.min.css">
+  <!-- Bootstrap Color Picker -->
+  <link rel="stylesheet" href="{{ asset('frontend') }}/bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css">
+  <!-- Bootstrap time Picker -->
+  <link rel="stylesheet" href="{{ asset('frontend') }}/plugins/timepicker/bootstrap-timepicker.min.css">
 
 
   <!-- Google Font -->
@@ -284,6 +294,79 @@
 <script src="{{ asset('frontend') }}/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('frontend') }}/dist/js/demo.js"></script>
+<!-- InputMask -->
+<script src="{{ asset('frontend') }}/plugins/input-mask/jquery.inputmask.js"></script>
+<script src="{{ asset('frontend') }}/plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
+<script src="{{ asset('frontend') }}/plugins/input-mask/jquery.inputmask.extensions.js"></script>
+<!-- Select2 -->
+<script src="{{ asset('frontend') }}/bower_components/select2/dist/js/select2.full.min.js"></script>
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Datemask dd/mm/yyyy
+    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+    //Datemask2 mm/dd/yyyy
+    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+    //Money Euro
+    $('[data-mask]').inputmask()
+
+    //Date range picker
+    $('#reservation').daterangepicker()
+    //Date range picker with time picker
+    $('#reservationtime').daterangepicker({ timePicker: true, timePickerIncrement: 30, locale: { format: 'MM/DD/YYYY hh:mm A' }})
+    //Date range as a button
+    $('#daterange-btn').daterangepicker(
+      {
+        ranges   : {
+          'Today'       : [moment(), moment()],
+          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: moment().subtract(29, 'days'),
+        endDate  : moment()
+      },
+      function (start, end) {
+        $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+      }
+    )
+
+    //Date picker
+    $('#datepicker').datepicker({
+      autoclose: true
+    })
+
+    //iCheck for checkbox and radio inputs
+    $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+      checkboxClass: 'icheckbox_minimal-blue',
+      radioClass   : 'iradio_minimal-blue'
+    })
+    //Red color scheme for iCheck
+    $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+      checkboxClass: 'icheckbox_minimal-red',
+      radioClass   : 'iradio_minimal-red'
+    })
+    //Flat red color scheme for iCheck
+    $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+      checkboxClass: 'icheckbox_flat-green',
+      radioClass   : 'iradio_flat-green'
+    })
+
+    //Colorpicker
+    $('.my-colorpicker1').colorpicker()
+    //color picker with addon
+    $('.my-colorpicker2').colorpicker()
+
+    //Timepicker
+    $('.timepicker').timepicker({
+      showInputs: false
+    })
+  })
+</script>
 </body>
 </html>
 
--- resources/views/registroCotizacion.blade.php
+++ resources/views/registroCotizacion.blade.php
@@ -4,55 +4,123 @@
     <div class="content-wrapper">
         <section class="content">
         <div class="row">
-                <!-- right col (We are only adding the ID to make the widgets sortable)-->
-                <section class="col-lg-12 connectedSortable">
-
-                    <div class="box-body">
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"
-                                    class=""></li>
-                  <li data-target=" #carousel-example-generic"
-                                    data-slide-to="2" class=""></li>
-                </ol>
-                <div class="
-                                    carousel-inner">
-                                    <div class="item active">
-                                        <img src="{{ asset('frontend') }}/dist/img/carousel/lote1.jpg" alt="First slide">
-
-                                        <div class="carousel-caption">
-                                            Primer Imagen
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="{{ asset('frontend') }}/dist/img/carousel/green.jpg" alt="Second slide">
-
-                                        <div class="carousel-caption">
-                                            Segunda Imagen
-                                        </div>
-                                    </div>
-                                    <div class="item">
-                                        <img src="{{ asset('frontend') }}/dist/img/carousel/arboles.jpg"
-                                            alt="Third slide">
-
-                                        <div class="carousel-caption">
-                                            Tercera Imagen
-                                        </div>
-                                    </div>
-                        </div>
-                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                            <span class="fa fa-angle-left"></span>
-                        </a>
-                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                            <span class="fa fa-angle-right"></span>
-                        </a>
-                    </div>
+            <!-- Horizontal Form -->
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3 class="box-title">Registrar Cotización</h3>
             </div>
+            <!-- /.box-header -->
+<!-- SELECT2 EXAMPLE -->
+        <div class="box box-default">
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <div class="row">
+            <div class="col-md-6">
+            <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Cliente</label>
 
-        </section>
-        <!-- right col -->
-    </div>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Cliente">
+                  </div>
+                </div>
+                <br><br>
+              <!-- /.form-group -->
+              <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Fecha Pago</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
+                  </div>
+                </div>
+                <br><br>
+              <!-- /.form-group -->
+              <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Lote</label>
+
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Lote">
+                  </div>
+                </div>
+              <!-- /.form-group -->
+            </div>
+            <!-- /.col -->
+
+            <div class="col-md-6">
+            <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Método Pago</label>
+
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Método Pago">
+                  </div>
+                </div>
+                <br><br>
+              <!-- /.form-group -->
+              <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">N° Cuotas</label>
+
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="N° Cuotas">
+                  </div>
+                </div>
+                <br><br>
+              <!-- /.form-group -->
+              <div class="form-group">
+                  <label for="precio" class="col-sm-2 control-label">Precio</label>
+                  <div class="col-sm-3">
+                    <input type="text" class="form-control" id="precio" placeholder="Precio">
+                  </div>
+                  <div>
+                  <label for="precioInicial" class="col-sm-2 control-label">Inicial</label>
+                  <div class="col-sm-3">
+                    <input type="text" class="form-control" id="precioInicial" placeholder="Inicial">
+                  </div>
+                  </div>
+                </div>
+                <br><br>
+              <!-- /.form-group -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.box-body -->
+        <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tr>
+                  <th>Descripción</th>
+                  <th>Precio</th>
+                  <th>Acción</th>
+                </tr>
+                <tr>
+                  <td>Lote 15 - Juan de la Rosa</td>
+                  <td>15297.63</td>
+                  <td><button type="button" class="btn btn-block btn-danger btn-xs">Eliminar</button></td>
+                </tr>
+              </table>
+              <br>
+              <div class="form-group">
+                  <label class="col-sm-2 control-label">TEA: </label>
+                  <label style="font-weight:normal">12.6825%</label>
+                <div>
+                  <label class="col-sm-2 control-label">Cuota Mensual:</label>
+                  <label style="font-weight:normal">470.73</label>
+                </div>
+              <div>
+                  <label class="col-sm-2 control-label">Total Inicial:</label>
+                  <label style="font-weight:normal">4000.00</label>
+              </div>
+              <div>
+                  <label class="col-sm-2 control-label">Total Venta:</label>
+                  <label style="font-weight:normal">15297.63</label>
+              </div>
+              </div>
+              <button type="button" class="btn btn-block btn-primary btn-xs col-sm-2" style="width:80px;height:30px;margin-right: 5px;">Guardar</button>
+              <button type="button" class="btn btn-block btn-warning btn-xs" style="width:80px;height:30px;">Cancelar</button>
+            </div>
+      </div>
+      <!-- /.box -->
+          </div>
+          <!-- /.box -->
         </section>
     </div>
 @endsection
